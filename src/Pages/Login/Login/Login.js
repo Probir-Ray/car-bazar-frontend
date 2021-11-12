b92d70690d88c8402b3d09426742c16492808083
Probir-Ray <somironroy.need@gmail.com>
2021-11-12 12:42:58 +0600

    update

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
 
 const Login = () => {
+    const [loginInfo, setLoginInfo] = useState({});
+
+    const handleChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginInfo = {...loginInfo};
+        newLoginInfo[field] = value;
+        setLoginInfo(newLoginInfo);
+    }
+
+    const handleLogin = e => {
+        alert('hello, all')
+        e.preventDefault();
+    }
     return (
     <>
         <Navigation/>
         <div className='my-4 py-4 container text-center box-container'>
             <h2>Login Page</h2>
-            <form className="row g-3 w-50 mx-auto mt-2 mb-4">
+            <form onSubmit={handleLogin} className="row g-3 w-50 mx-auto mt-2 mb-4">
                 <div className="col-md-12">
-                    <input type="text" className="form-control py-2 mb-2" placeholder="Enter Your Email"/>
+                    <input type="text" name="email" className="form-control py-2 mb-2" onChange={handleChange} placeholder="Enter Your Email"/>
                 </div>
                 <div className="col-md-12">
-                    <input type="password" className="form-control py-2 mb-2" placeholder="Enter Your Password"/>
+                    <input type="password" name="password" className="form-control py-2 mb-2" onChange={handleChange} placeholder="Enter Your Password"/>
                 </div>
                 <div className="col-12">
                     <button type="submit" className="form-control btn btn-primary py-2 mb-2">Login</button>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../Shared/Footer/Footer';
 import Navigation from '../Shared/Navigation/Navigation';
 
 const Register = () => {
+    const [loginInfo, setLoginInfo] = useState({});
+
+    const handleChange = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginInfo = {...loginInfo};
+        newLoginInfo[field] = value;
+        setLoginInfo(newLoginInfo);
+    }
+
+    const handleSignIn = e => {
+        if(loginInfo.password !== loginInfo.password2) {
+            alert("Password didn't match");
+            return;
+        }
+        alert('hello, all')
+        e.preventDefault();
+    }
+
     return (
         <>
         <Navigation></Navigation>
         <div className='container my-5 text-center'>
             <h2 className='my-4'>Registration Page</h2>
 
-            <form className="row g-3 w-50 mx-auto mt-2 mb-4">
+            <form onSubmit={handleSignIn} className="row g-3 w-50 mx-auto mt-2 mb-4">
                 <div className="col-md-12">
-                    <input type="email" className="form-control py-2 mb-2" placeholder="Enter Your Email" required/>
+                    <input type="email" name="email" className="form-control py-2 mb-2" placeholder="Enter Your Email" onChange={handleChange} required/>
                 </div>
                 <div className="col-md-12">
-                    <input type="password" className="form-control py-2 mb-2" placeholder="Enter Your Password" required/>
+                    <input type="password" name="password" className="form-control py-2 mb-2" placeholder="Enter Your Password" onChange={handleChange} required/>
+                </div>
+                <div className="col-md-12">
+                    <input type="password" name="password2" className="form-control py-2 mb-2" placeholder="Retype Your Password" onChange={handleChange} required/>
                 </div>
                 <div className="col-12">
                     <button type="submit" className="form-control btn btn-primary form-control py-2 mb-2">Register</button>
