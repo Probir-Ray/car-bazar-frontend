82f4ae6431168d8d858146e9f8f711691a56acaa
Probir-Ray <somironroy.need@gmail.com>
2021-11-14 21:50:29 +0600

    update

--- src/Pages/Dashboard/DashboardHome/DashboardHome.js
+++ src/Pages/Dashboard/DashboardHome/DashboardHome.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DashboardHome = () => {
     return (
         <div style={{marginTop: '18%'}}>
-            <h1 className="py-3 text-center">Welcome, to Car bazar Dashboard</h1>
+            <h1 className="py-3 text-center">Welcome to Car bazar Dashboard</h1>
         </div>
     );
 };
--- src/Pages/Home/Review/Review.js
+++ src/Pages/Home/Review/Review.js
@@ -4,12 +4,13 @@ const Review = (props) => {
     const {name, desc, img, location} = props.review;
     return (
         
-            <div className="col col-md-6">
+            <div className="col col-md-6 text-center">
                 <div className="card p-4 m-3" style={{backgroundColor: 'rgba(249, 249, 249, 0.95)'}}>
                     <img src={img} className="card-img-top w-25 mx-auto" style={{borderRadius: '15px', width: 150, height: 150, objectFit: "cover", marginTop: '-10%'}} alt={name}/>
                     <div className="card-body">
                         <h6 className="card-text text-gray">{desc}</h6>
-                        <h5 className="card-title">{name}</h5>
+                        <hr/>
+                        <h5 className="card-title my-3">{name}</h5>
                         <p className="card-text">{location}</p>
                     </div>
                 </div>
